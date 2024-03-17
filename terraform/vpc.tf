84102973203fdddfe6c88496da70b282e4ff8020
Karthik <karthikkumardevops9@gmail.com>
2024-03-17 01:11:02 -0400

    updated security port to 0

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -47,22 +47,22 @@ resource "aws_security_group" "ec2_security_group" {
 
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     self        = "false"
     cidr_blocks = ["0.0.0.0/0"]
     description = "ContainerPort"
   }
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "icmp"
-    self        = "false"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "ContainerPort"
-  }
+  # ingress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "icmp"
+  #   self        = "false"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  #   description = "ContainerPort"
+  # }
 
   egress {
     from_port   = 0
@@ -94,15 +94,7 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "HttpPortForALB"
   }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    self        = "false"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "ContainerPort"
-  }
+  
 
  egress {
     from_port   = 0
